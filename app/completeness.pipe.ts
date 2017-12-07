d66a6d852e5abc830c5582abce6eabd5f1f004e8
hansen hansen and sravanthi velaga <sravanthi@>
2017-12-06 16:03:38 -0800

    added components and pipes

---
+++ app/completeness.pipe.ts
@@ -0,0 +1,42 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Task} from './task.model';
+
+@Pipe({
+  name: "completeness",
+    pure: false
+})
+
+export class CompletenessPipe implements PipeTransform {
+
+  // transform(input: Task[], args) {
+  //   var output: Task[] = [];
+  //   for (var i = 0; i < input.length; i++) {
+  //     if (input[i].done === false) {
+  //       output.push(input[i]);
+  //     }
+  //   }
+  //   return input;
+  // }
+
+  transform(input: Task[], desiredCompleteness) {
+    var output: Task[] = [];
+    if(desiredCompleteness === "incompleteTasks") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].done === false) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (desiredCompleteness === "completedTasks") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].done === true) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
+    }
+  }
+
+}
---
+++ app/task.model.ts
@@ -0,0 +1,10 @@
+export class Task {
+  public done: boolean = false;
+  // public ingredient: string[] = [];
+  constructor(public description: string, public priority: number) { }
+
+  // addIngredient(ingredientParam: string){
+  //   this.ingredient.push(ingredientParam);
+  // }
+
+}
